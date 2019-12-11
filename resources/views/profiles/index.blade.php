bf29af2870afda576ffeed543c1eaa41a8effe24
Marko Radulovic <upss070288@gmail.com>
2019-12-11 18:52:00 +0100

    First push - setting single post page

---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 pb-5">
+            <img src="https://instagram.fbeg1-1.fna.fbcdn.net/v/t51.2885-19/s150x150/70985486_577637296311063_2240788552625422336_n.jpg?_nc_ht=instagram.fbeg1-1.fna.fbcdn.net&oh=adf87dc753ceb4389b2d934aae6f4f5e&oe=5E82A0D7" alt="logo" class="rounded-circle">
+        </div>
+        <div class="col-9 pt-5">
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{$user->username}}</h1>
+                <a href="/p/create">Add New Post</a>
+            </div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        @foreach($user->posts as $post)
+            <div class="col-4 pb-4">
+                <a href="/p/{{ $post->id }}">
+                    <img src="/storage/{{ $post->image }}" class="w-100" alt="">
+                </a>
+            </div>
+        @endforeach
+    </div>
+</div>
+@endsection
