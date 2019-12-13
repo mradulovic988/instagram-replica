cb44ed2defd49b62d10a2e84f3a18f32180379d2
Marko Radulovic <upss070288@gmail.com>
2019-12-13 22:37:31 +0100

    Fixed issue with No profile photo

--- app/Profile.php
+++ app/Profile.php
@@ -8,6 +8,11 @@ class Profile extends Model
 {
     protected $guarded = [];
 
+    public function profileImage() {
+        $imagePath = ($this->image) ? '/storage/' . $this->image : 'profile/no_image.png';
+        return '/storage/' . $imagePath;
+    }
+
     public function user() {
         return $this->belongsTo(User::class);
     }
