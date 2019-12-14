ca722b70bca62fbd5b3956111db727461a5059f9
Marko Radulovic <upss070288@gmail.com>
2019-12-14 11:10:31 +0100

    Add many to many relationship for the Follow function

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -10,7 +10,7 @@
             <div class="d-flex justify-content-between align-items-baseline">
                 <div class="d-flex align-items-center pb-3">
                     <div class="h4">{{$user->username}}</div>
-                    <follow-button></follow-button>
+                    <follow-button user-id="{{$user->id}}"></follow-button>
                 </div>
                 @can('update', $user->profile)
                     <a href="/p/create">Add New Post</a>
